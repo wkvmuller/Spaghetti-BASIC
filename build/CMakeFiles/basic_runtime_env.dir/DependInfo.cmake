0f02f1d77f25b16cc8175577bd76cddeb6066823
wkvmuller <wmklaushaus@gmail.com>
2025-04-20 00:39:34 -0400

    update cleng-format all files......

---
+++ build/CMakeFiles/basic_runtime_env.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wkvmuller/scp/SB_GET/basic_runtime_env/src/basic_runtime_env.cpp" "CMakeFiles/basic_runtime_env.dir/src/basic_runtime_env.cpp.o" "gcc" "CMakeFiles/basic_runtime_env.dir/src/basic_runtime_env.cpp.o.d"
+  "/home/wkvmuller/scp/SB_GET/basic_runtime_env/src/interpreter.cpp" "CMakeFiles/basic_runtime_env.dir/src/interpreter.cpp.o" "gcc" "CMakeFiles/basic_runtime_env.dir/src/interpreter.cpp.o.d"
+  "/home/wkvmuller/scp/SB_GET/basic_runtime_env/src/renumber.cpp" "CMakeFiles/basic_runtime_env.dir/src/renumber.cpp.o" "gcc" "CMakeFiles/basic_runtime_env.dir/src/renumber.cpp.o.d"
+  "/home/wkvmuller/scp/SB_GET/basic_runtime_env/src/syntax.cpp" "CMakeFiles/basic_runtime_env.dir/src/syntax.cpp.o" "gcc" "CMakeFiles/basic_runtime_env.dir/src/syntax.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
